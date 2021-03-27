b107b6acef7e234f72683634ac9ddb671db1fa85
caiopawilak@gmail.com <39964779+CaioPawilak@users.noreply.github.com>
2021-03-27 00:38:03 -0300

    Removing files

--- src/modules/cars/useCases/importCategory/importCategoryController.ts
+++ src/modules/cars/useCases/importCategory/importCategoryController.ts
@@ -1,10 +1,18 @@
-class importCategoryController {
-    
-    constructor(){}
+import { Request, Response } from 'express'
+import { ImportCategoryUseCase } from './importCategoryUseCase'
 
-    execute()
+class ImportCategoryController {
+
+    constructor(private importCategoryUseCase: ImportCategoryUseCase) { }
+
+    handle(request: Request, response: Response): Response {
+        const { file } = request;
+        this.importCategoryUseCase.execute(file)
+        return response.send()
+
+    }
+
 }
 
 
-
-export {importCategoryController}
+export { ImportCategoryController }
