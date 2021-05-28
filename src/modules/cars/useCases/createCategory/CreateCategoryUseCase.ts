ca6a689c765cd70f07d09412ee73982ed3e0a5c1
caiopawilak@gmail.com <39964779+CaioPawilak@users.noreply.github.com>
2021-05-28 19:38:14 -0300

    TSyringe

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -1,14 +1,17 @@
 import { ICategoriesRepository } from "../../repositories/implementations/ICategoriesRepository"
+import {inject, injectable} from "tsyringe"
 
 
 interface IRequest {
     name: string;
     description: string;
 }
-
+@injectable()
 class CreateCategoryUseCase {
 
-    constructor(private categoryRepo : ICategoriesRepository){ }
+    constructor(
+        @inject("CategoriesRepository")
+        private categoryRepo : ICategoriesRepository){ }
     async execute({name, description}: IRequest): Promise<void>{
         const categoryAlreadyExists = await this.categoryRepo.findByName(name)
 
--- src/modules/cars/useCases/createCategory/index.ts
+++ src/modules/cars/useCases/createCategory/index.ts
@@ -1,18 +1,18 @@
 
-import { CategoriesRepository } from "../../repositories/CategoriesRepository";
-import { CreateCategoryController } from "./CreateCategoryController";
-import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
+// import { CategoriesRepository } from "../../repositories/CategoriesRepository";
+// import { CreateCategoryController } from "./CreateCategoryController";
+// import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 
-export default (): CreateCategoryController => {
-    const categoriesRepository = new CategoriesRepository();
-    const createCategoryUserCase = new CreateCategoryUseCase(categoriesRepository)
+// export default (): CreateCategoryController => {
+//     const categoriesRepository = new CategoriesRepository();
+//     const createCategoryUserCase = new CreateCategoryUseCase(categoriesRepository)
 
-    const createCategoryController = new CreateCategoryController(createCategoryUserCase)
+//     const createCategoryController = new CreateCategoryController(createCategoryUserCase)
 
 
-    return createCategoryController;
+//     return createCategoryController;
 
-}
+// }
 
 
 
